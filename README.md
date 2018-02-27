9c587980a8b71434bd4773add4c6e06317177b30
Maho Morichika <nat@magicnat.com>
2018-02-27 17:47:09 -0500

    init.

---
+++ README.md
@@ -0,0 +1,9 @@
+neimap: Interactive BGP neighbor map
+---
+
+Yet another "Oh this class is so damn boring I got to find something to do" project.
+
+This is an interactive BGP neighbor relationship map. It uses [RIPE Stat API](https://stat.ripe.net/docs/data_api) to obtain BGP neighbor information. 
+
+To use it, visit [lab.nat.moe/neimap](http://lab.nat.moe/neimap). Supply an ASN to get started, then click on any node to draw their neighbors on the map.
+
---
+++ js/app.js
@@ -0,0 +1,51 @@
+var getNei = asn => new Promise((res, rej) => {
+
+  var api_pre = 'https://stat.ripe.net/data/asn-neighbours/data.json?resource=';
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', api_pre + asn);
+  xhr.onload = function () {
+    if (this.status == 200) {
+      var data = JSON.parse(xhr.response);
+      if (data.status != "ok") rej({ok: false, err: 'API not OK.'});
+      res({ok: true, nei: data.data.neighbours});
+    } else rej({ok: false, err: xhr.statusText});
+  };
+  xhr.send();
+
+});
+
+var container = document.getElementById('display');
+var working = document.getElementById('working');
+var nodes = new vis.DataSet();
+var edges = new vis.DataSet();
+var garph = new vis.Network(container, {nodes, edges}, {});
+var visited = [];
+
+var addNode = function(asn, dst) {
+  if(!dst.get().filter(n => n.id == asn).length)
+    dst.add({id: asn, label: "AS" + asn, chosen: {
+      node: (values, id) => { drawNei(id); }
+    }});
+}
+
+var addEdge = function(n1, n2, dst) {
+  if(!dst.get().filter(e => (e.from == n1 && e.to == n2) || (e.from == n2 && e.to == n1)).length)
+    dst.add({from: n1, to: n2});
+}
+
+async function drawNei(asn) {
+  if(visited.includes(asn)) return;
+  working.className = '';
+  var nei = await getNei(asn);
+  working.className = 'hide';
+  if(!nei.ok) return;
+  visited.push(asn);
+  addNode(asn, nodes);
+  nei.nei.forEach(n => {
+    addNode(n.asn, nodes);
+    addEdge(asn, n.asn, edges);
+  });
+}
+
+drawNei(Number.parseInt(prompt('ASN to get started')));
